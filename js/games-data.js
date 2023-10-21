2b592a23ff2fbe1dda93321526b033c4449a9f27
Saykes <114241448+henrysion@users.noreply.github.com>
2023-10-21 09:46:48 +0800

    ss

--- js/games-data.js
+++ js/games-data.js
@@ -10,4 +10,10 @@ var gamesData = [ {
 	"name": "Ultimate Vocal Remover",
 	"tags": ["音频处理"],
 	"id": "d1eeb899810e4c359805ad860e405847"
+},{
+	"url": "https://waifu2x.box.ttteam.top/",
+	"imgSrc": "/imgs/waifu2x.png",
+	"name": "Waifu2x",
+	"tags": ["图像处理"],
+	"id": "d1eeb899810e4c359805ad860e405847"
 }]
